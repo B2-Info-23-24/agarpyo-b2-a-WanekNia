3d7e82c6df41bd0c1d3f6ce2bba2c805a2b28fbc
Wanek <nathan.wanecque@outlook.com>
2024-02-11 20:10:10 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/3a9G6PPC)
+
+# Agarpyo
+
+Generic Aragio like with Python and Pygame
+
+## Clone the project
+```
+git clone https://github.com/B2-Info-23-24/agarpyo-b2-a-WanekNia.git
+```
+
+## Installation
+
+you need to have 'Anaconda' in your system : 'https://www.anaconda.com/download'
+
+in Anaconda prompt :
+```bash
+conda create --name agarpyo python=3.8
+conda activate agarpyo
+pip install pygame
+```
+
+## Usage
+
+Directly in the anaconda prompt
+```
+conda activate agarpyo
+cd [clone directory]
+python menu.py
+```
--- game.py
+++ game.py
@@ -6,35 +6,34 @@ import math
 class Game:
     def __init__(self, nbrFood=5, nbrPiege=2, control_mode="Clavier"):
         self.control_mode = control_mode
-        # Initialisation de Pygame
+        # Init
         pygame.init()
-        self.debug_font = pygame.font.Font(None, 24)  # Crée une police de taille 24
+        self.debug_font = pygame.font.Font(None, 24)  # Police
 
-        # Définition de la taille de la fenêtre
+        # GameSize
         self.width, self.height = 1280, 720
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Agario-like")
 
-        # Création du joueur
+        # Create Pawn
         self.player = Player(self.width // 2, self.height // 2 , self.control_mode)
-        # Groupe de sprites pour gérer le joueur
         self.all_sprites = pygame.sprite.Group()
         self.all_sprites.add(self.player)
 
-        # Création d'un groupe de sprites pour les entités
-        self.entities = pygame.sprite.Group()  # Modifié pour être un attribut de l'instance
+        # SpriteGroup Entity (food)
+        self.entities = pygame.sprite.Group()
 
-        # Création d'un groupe de sprites pour les pièges
+        # SpriteGroup Piege
         self.pieges = pygame.sprite.Group()
 
-        # Ajouter quelques entités à des positions aléatoires
-        for _ in range(nbrFood):  # Créer 10 entités pour l'exemple
+        # Add Food
+        for _ in range(nbrFood):
             x = random.randint(0, self.width)
             y = random.randint(0, self.height)
             entity = Entity(x, y)
-            self.entities.add(entity)  # Ajoute les entités au groupe
+            self.entities.add(entity)
 
-        # Générer les pièges
+        # Add piege
         for _ in range(nbrPiege):
             x = random.randint(70, 1200)
             y = random.randint(50, 700)
@@ -50,54 +49,51 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-            # Mise à jour des sprites
+            # Update sprite
             self.all_sprites.update()
-            self.entities.update()  # Ajouté pour mettre à jour les entités, si nécessaire
+            self.entities.update()
 
-            # Détection de collision entre le joueur et les entités
+            # Colision Pawn<->Entity
             for entity in self.entities:
                 if pygame.sprite.collide_circle(self.player, entity):
                     self.player.mange(entity)
                     entity.reposition()
 
-            # Détection de collision entre le joueur et les pièges
+            # Colision Pawn<->Piege
             for piege in self.pieges:
                 if pygame.sprite.collide_circle(self.player, piege) and piege.radius < self.player.radius:
-                    # Logique pour terminer le jeu
+                    # End Game
                     self.show_end_screen()
                     pygame.quit()
                     sys.exit()
 
-            # Effacement de l'écran
             self.screen.fill((128, 128, 128))
 
-            # Affichage des sprites
+            # Show sprite
             self.all_sprites.draw(self.screen)
             self.entities.draw(self.screen)  # Ajouté pour dessiner les entités sur l'écran
             self.pieges.draw(self.screen)
 
-            # Dessiner les informations de debug
+            # Debug
             self.draw_debug_info()
 
-            # Rafraîchissement de l'écran
+            # Refresh
             pygame.display.flip()
 
-            # Contrôle de la fréquence d'images
+            # Frequency
             clock.tick(60)
 
 
     def draw_debug_info(self):
-        # Changer la couleur du texte pour qu'il soit visible sur un fond blanc
-        debug_color = (0, 0, 0)  # Noir, pour un bon contraste sur fond blanc
-        # Exemple d'informations à afficher
+        debug_color = (0, 0, 0) 
         if self.control_mode == "Souris":
             control_text = "Mouse"
         elif self.control_mode == "Clavier":
             control_text = "Z Q S D"
         debug_text = f"Score: {self.player.score}"
         debug_text = f"Player Position: {self.player.rect.center} | Control: {control_text}"
-        debug_surf = self.debug_font.render(debug_text, True, debug_color)  # Utiliser la couleur noire pour le texte
-        self.screen.blit(debug_surf, (10, 10))  # Dessine en haut à gauche de l'écran
+        debug_surf = self.debug_font.render(debug_text, True, debug_color)
+        self.screen.blit(debug_surf, (10, 10))
 
     def show_end_screen(self):
         BLACK = (0, 0, 0)
@@ -133,18 +129,18 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(x, y))
         self.speed = 1
         self.score = 0
-        self.control_mode = control_mode  # Ajout du mode de contrôle comme attribut
+        self.control_mode = control_mode
 
     def update(self):
         if self.control_mode == "Souris":
             mouse_x, mouse_y = pygame.mouse.get_pos()
             dx = mouse_x - self.rect.centerx
             dy = mouse_y - self.rect.centery
-            distance = math.sqrt(dx**2 + dy**2)  # Calculer la distance euclidienne
+            distance = math.sqrt(dx**2 + dy**2)  # distance truc j'ai pa compris
 
-            threshold = 10  # Définir une marge d'erreur ou une "zone morte"
+            threshold = 10 # marge d'erreur
 
-            if distance > threshold:  # Se déplacer seulement si en dehors de la zone morte
+            if distance > threshold:
                 angle = math.atan2(dy, dx)
                 self.rect.x += self.speed * math.cos(angle)
                 self.rect.y += self.speed * math.sin(angle)
@@ -162,7 +158,7 @@ class Player(pygame.sprite.Sprite):
             self.rect.x += dx
             self.rect.y += dy
 
-        # Gestion des bords de l'écran
+        # TP
         if self.rect.top <= 0:
             self.rect.bottom = 719
         elif self.rect.bottom >= 720:
@@ -174,11 +170,11 @@ class Player(pygame.sprite.Sprite):
 
     
     def mange(self, entity):
-        self.radius += 2  # Augmenter le rayon de 2
-        self.speed += 0.2  # Augmenter la vitesse de 5
+        self.radius += 2  # Augmenter le rayon
+        self.speed += 0.2  # Augmenter la vitesse 
         self.score += 1
         
-        # Mettre à jour l'image pour refléter le nouveau rayon
+        #update le sprite
         self.image = pygame.Surface((self.radius*2, self.radius*2), pygame.SRCALPHA)
         pygame.draw.circle(self.image, self.color, (self.radius, self.radius), self.radius)
         self.rect = self.image.get_rect(center=self.rect.center)
@@ -193,15 +189,15 @@ class Entity(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(x, y))
     
     def reposition(self):
-        # Générer de nouvelles positions x et y aléatoirement sur le terrain
+        # x, Y rng
         new_x = random.randint(0, 1280)
         new_y = random.randint(0, 720)
         self.rect.center = (new_x, new_y)
 
 class Piege(pygame.sprite.Sprite):
-    def __init__(self, x, y, color=(255, 0, 0)):  # Garde color paramétrable avec une valeur par défaut
+    def __init__(self, x, y, color=(255, 0, 0)):
         super().__init__()
-        self.radius = random.randint(20, 70)  # Génère un rayon aléatoire entre 70 et 150
+        self.radius = random.randint(20, 70) 
         self.color = color
         self.image = pygame.Surface((self.radius*2, self.radius*2), pygame.SRCALPHA)
         pygame.draw.circle(self.image, self.color, (self.radius, self.radius), self.radius)
